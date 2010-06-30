c32ef7c6a1dc158ee3063ea9c901f9cab03239bf
j.a.koster@gmail.com <j.a.koster@gmail.com@f9376deb-ae59-c370-c37a-0523a45e0afc>
2010-06-30 12:41:56 +0000

    Repackaged and moved simpledotcomgame classes into this, the main project.

--- trunk/javasteam/src/main/java/nl/seetickets/javasteam/helloworld/App.java
+++ trunk/javasteam/src/main/java/nl/seetickets/javasteam/helloworld/App.java
@@ -1,4 +1,4 @@
-package nl.seetickets;
+package nl.seetickets.javasteam.helloworld;
 
 /**
  * Hello world!
---
+++ trunk/javasteam/src/test/java/nl/seetickets/javasteam/dotcomgame/SimpleDotComTestDrive.java
@@ -0,0 +1,33 @@
+package nl.seetickets.javasteam.dotcomgame;
+
+import java.util.ArrayList;
+
+public class SimpleDotComTestDrive {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		SimpleDotCom dot = new SimpleDotCom();
+		ArrayList<String> locations = new ArrayList<String>();
+		locations.add("2");
+		locations.add("3");
+		locations.add("4");
+
+		dot.setLocationCells(locations);
+
+		String testResult = "failed";
+
+		String userGuess = "1";
+		String result = dot.checkYourself(userGuess);
+
+		if (result == "hit") {
+			testResult = "passed";
+		} else if (result == "miss") {
+			testResult = "passed";
+		}
+
+		System.out.println(testResult);
+	}
+}
