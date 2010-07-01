95fca2f55c2b50fbaeb97fdeaa18ce3baccace29
martijn.lapre@gmail.com <martijn.lapre@gmail.com@f9376deb-ae59-c370-c37a-0523a45e0afc>
2010-07-01 06:32:00 +0000

    Refactored SimpleDotCom code

--- trunk/simpleDotComGame/src/main/java/nl/seetickets/SimpleDotCom.java
+++ trunk/simpleDotComGame/src/main/java/nl/seetickets/SimpleDotCom.java
@@ -1,21 +1,23 @@
 package nl.seetickets;
-import java.util.ArrayList;
+
+import java.util.List;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 public class SimpleDotCom {
 
-	private ArrayList<String> locationCells;
-	int numOfHits = 0;
-	
-	public String checkYourself(String userInput){
-		
+	private List<String> locationCells;
+
+	public String checkYourself(String userInput) {
+
 		String result = "miss";
 		int index = locationCells.indexOf(userInput);
-		
+
 		if (index >= 0) {
-			
+
 			locationCells.remove(index);
-			
-			if(locationCells.isEmpty()){
+
+			if (locationCells.isEmpty()) {
 				result = "kill";
 			} else {
 				result = "hit";
@@ -23,9 +25,15 @@ public class SimpleDotCom {
 		}
 		return result;
 	}
-	
-	public void setLocationCells(ArrayList<String> loc){
+
+	public void setLocationCells(List<String> loc) {
 		locationCells = loc;
 	}
-	
+
+	public boolean isValid(String userName) {
+		// throw new NotImplementedException();
+
+		return ("Martijn".equals(userName));
+	}
+
 }
