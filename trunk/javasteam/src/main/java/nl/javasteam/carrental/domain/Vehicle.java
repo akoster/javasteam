6cda165de6396be146666dfbf3afb597380a0107
martijn.lapre@gmail.com <martijn.lapre@gmail.com@f9376deb-ae59-c370-c37a-0523a45e0afc>
2010-07-15 09:28:27 +0000

    Refactored CarRental domain Vehicle class Added three annotations with getters and setters Used @Columnn (javax.persistence.column) - vehicleBusinessId - maxCargoWeightKilogram - rentalPricePerDayEuro

--- trunk/javasteam/src/main/java/nl/javasteam/carrental/domain/Vehicle.java
+++ trunk/javasteam/src/main/java/nl/javasteam/carrental/domain/Vehicle.java
@@ -1,5 +1,6 @@
 package nl.javasteam.carrental.domain;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -24,4 +25,39 @@ public class Vehicle {
 	public void setId(Long id) {
 		this.id = id;
 	}
+	
+	@Column(name="vehicleBusinessId", updatable=true, length=20, unique=true)
+	private String vehicleBusinessId;
+	
+	public String getVehicleBusinessId() {
+		return vehicleBusinessId;
+	}
+	
+	public void setVehicleBusinessId(String vehicleBusinessId) {
+		this.vehicleBusinessId = vehicleBusinessId;
+	}
+	
+	@Column(name="maxCargoWeightKiloGram", updatable=true, length=6, unique=false)
+	private int maxCargoWeightKiloGram;
+	
+	public int getMaxCargoWeightKiloGram() {
+		return maxCargoWeightKiloGram;
+	}
+	
+	public void setMaxCargoWeightKiloGram(int maxCargoWeightKiloGram) {
+		this.maxCargoWeightKiloGram = maxCargoWeightKiloGram;
+	}
+	
+	@Column(name="rentalPricePerDayEuro", updatable=true, unique=false)
+	private double rentalPricePerDayEuro;
+	
+	public double getRentalPricePerDayEuro() {
+		return rentalPricePerDayEuro;
+	}
+	
+	public void setRentalPricePerDayEuro(double rentalPricePerDayEuro){
+		this.rentalPricePerDayEuro = rentalPricePerDayEuro;
+	}
+	
+	
 }
