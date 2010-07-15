dc74af619334a20e773787b7c9f8ad3da5af01cc
martijn.lapre@gmail.com <martijn.lapre@gmail.com@f9376deb-ae59-c370-c37a-0523a45e0afc>
2010-07-15 21:26:52 +0000

    Added getters and setters and id annotation

--- javasteam/src/main/java/nl/javasteam/carrental/domain/VehicleType.java
+++ javasteam/src/main/java/nl/javasteam/carrental/domain/VehicleType.java
@@ -1,7 +1,34 @@
 package nl.javasteam.carrental.domain;
 
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
 enum VehicleType {
-	CAR,
-	VAN,
-	TRUCK;
+	CAR, VAN, TRUCK;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	@Enumerated(EnumType.STRING)
+	private VehicleType vehicleType;
+	
+	public VehicleType getVehicleType() {
+		return vehicleType;
+	}
+
+	public void setVehicleType(VehicleType vehicleType) {
+		this.vehicleType = vehicleType;
+	}
 }
