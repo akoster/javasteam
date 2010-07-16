37b671417b77a9224d02e1b816a685857be826f3
martijn.lapre@gmail.com <martijn.lapre@gmail.com@f9376deb-ae59-c370-c37a-0523a45e0afc>
2010-07-16 16:05:28 +0000

    Added JavaDoc Made the enum public

--- trunk/javasteam/src/main/java/nl/javasteam/carrental/domain/VehicleType.java
+++ trunk/javasteam/src/main/java/nl/javasteam/carrental/domain/VehicleType.java
@@ -1,36 +1,10 @@
 package nl.javasteam.carrental.domain;
 
-import javax.persistence.Column;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.Id;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-
-enum VehicleType {
+/**
+ * VehicleTypes
+ * 
+ * @author mlapre
+ */
+public enum VehicleType {
 	CAR, VAN, TRUCK;
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Long id;
-	
-	public Long getId() {
-		return id;
-	}
-	
-	public void setId(Long id) {
-		this.id = id;
-	}
-	
-	@Column(name="vehicleType")
-	@Enumerated(EnumType.STRING)
-	private VehicleType vehicleType;
-	
-	public VehicleType getVehicleType() {
-		return vehicleType;
-	}
-
-	public void setVehicleType(VehicleType vehicleType) {
-		this.vehicleType = vehicleType;
-	}
 }
