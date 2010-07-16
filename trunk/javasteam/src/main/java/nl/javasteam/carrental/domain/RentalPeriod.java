167cdd2e2c95783f94edc5742a4be20a4571bbf2
martijn.lapre@gmail.com <martijn.lapre@gmail.com@f9376deb-ae59-c370-c37a-0523a45e0afc>
2010-07-16 15:58:09 +0000

    Added JavaDoc Added the @Embeddable annotation

--- trunk/javasteam/src/main/java/nl/javasteam/carrental/domain/RentalPeriod.java
+++ trunk/javasteam/src/main/java/nl/javasteam/carrental/domain/RentalPeriod.java
@@ -2,30 +2,20 @@ package nl.javasteam.carrental.domain;
 
 import java.util.Date;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.Embeddable;
 
-@Entity
+/**
+ * A rentalPeriod
+ * 
+ * @author mlapre
+ */
+@Embeddable
 public class RentalPeriod {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Long id;
-	
-	public Long getId() {
-		return id;
-	}
-	
-	public void setId(Long id) {
-		this.id = id;
-	}
-	
-	@Column(name="startDate")
 	private Date startDate;
-	
+	private Date endDate;
+
+
 	public Date getStartDate() {
 		return startDate;
 	}
@@ -33,9 +23,6 @@ public class RentalPeriod {
 	public void setStartDate(Date startDate) {
 		this.startDate = startDate;
 	}
-	
-	@Column(name="endDate")
-	private Date endDate;
 	
 	public Date getEndDate() {
 		return endDate;
