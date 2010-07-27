ea722b43b49759c9e77e77c47244d2321616faa6
martijn.lapre@gmail.com <martijn.lapre@gmail.com@f9376deb-ae59-c370-c37a-0523a45e0afc>
2010-07-27 13:22:37 +0000

    Refactored Added a constructor

---
+++ trunk/javasteam/src/main/java/nl/javasteam/carrental/swing/TabsPane.java
@@ -0,0 +1,18 @@
+package nl.javasteam.carrental.swing;
+
+
+import javax.swing.JTabbedPane;
+
+/**
+ * Tabs for the user interface
+ * 
+ * @author mlapre
+ */
+public class TabsPane extends JTabbedPane {
+	
+	private static final long serialVersionUID = 1L;
+
+	public TabsPane() {
+		addTab("Vehicle", new VehiclePanel());
+	}	
+}
