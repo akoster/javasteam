072089c3347482f69c97976128b10cbe205b14fc
martijn.lapre@gmail.com <martijn.lapre@gmail.com@f9376deb-ae59-c370-c37a-0523a45e0afc>
2010-08-03 11:45:31 +0000

    Initial commit

---
+++ trunk/javasteam/src/main/java/nl/javasteam/carrental/data/CustomerDao.java
@@ -0,0 +1,24 @@
+package nl.javasteam.carrental.data;
+
+import java.util.Date;
+import java.util.List;
+
+import nl.javasteam.carrental.domain.Customer;
+import nl.javasteam.carrental.domain.DriversLicense;
+
+/**
+ * 
+ * @author mlapre
+ */
+public interface CustomerDao extends BaseDao<Customer, Long> {
+
+	// Provided by BaseDao:
+	// --Enter new customer
+	// --Find customer by id
+	// --Update customer
+	// --List all customers
+
+	List<Customer> listCustomer(String code, String firstName, String lastName,
+			String homeAddress, int phoneNumber, int bankAccountNumber, Date birthDate,
+			Object driversLicense);
+}
