2bcda0b4d07ce2072480a776e5e9764face83a77
martijn.lapre@gmail.com <martijn.lapre@gmail.com@f9376deb-ae59-c370-c37a-0523a45e0afc>
2010-08-03 11:45:13 +0000

    Initial commit

---
+++ javasteam/src/main/java/nl/javasteam/carrental/data/hibernate/HibernateCustomerDao.java
@@ -0,0 +1,27 @@
+package nl.javasteam.carrental.data.hibernate;
+
+import java.util.Date;
+import java.util.List;
+
+import nl.javasteam.carrental.data.CustomerDao;
+import nl.javasteam.carrental.domain.Customer;
+
+/**
+ * 
+ * @author mlapre
+ */
+public class HibernateCustomerDao extends HibernateBaseDao<Customer, Long> 
+	implements CustomerDao {
+
+	public HibernateCustomerDao() {
+		super(Customer.class);
+	}
+
+	public List<Customer> listCustomer(String code, String firstName,
+			String lastName, String homeAddress, int phoneNumber,
+			int bankAccountNumber, Date birthDate, Object driversLicense) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
