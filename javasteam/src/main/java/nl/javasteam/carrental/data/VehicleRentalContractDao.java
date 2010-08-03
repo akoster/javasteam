3b898fd05b3171f544afbb153ccbb996010f7b7e
martijn.lapre@gmail.com <martijn.lapre@gmail.com@f9376deb-ae59-c370-c37a-0523a45e0afc>
2010-08-03 11:45:43 +0000

    Initial commit

---
+++ javasteam/src/main/java/nl/javasteam/carrental/data/VehicleRentalContractDao.java
@@ -0,0 +1,11 @@
+package nl.javasteam.carrental.data;
+
+import nl.javasteam.carrental.domain.VehicleRentalContract;
+
+/**
+ * 
+ * @author mlapre
+ */
+public interface VehicleRentalContractDao extends BaseDao<VehicleRentalContract, Long> {
+
+}
