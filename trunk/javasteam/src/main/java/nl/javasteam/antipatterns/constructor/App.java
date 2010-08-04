6632c4d9c93a524bc6d514dbbf7e946d88a6f9ed
j.a.koster@gmail.com <j.a.koster@gmail.com@f9376deb-ae59-c370-c37a-0523a45e0afc>
2010-08-04 10:31:35 +0000

    Improved antipattern example.

--- trunk/javasteam/src/main/java/nl/javasteam/antipatterns/constructor/App.java
+++ trunk/javasteam/src/main/java/nl/javasteam/antipatterns/constructor/App.java
@@ -4,10 +4,17 @@ public class App {
 
 	public static void main(String[] args) {
 
-		System.out.println("BUILD AN ANIMAL :");
+		System.out.println("new Animal:");
 		new Animal();
 
-		System.out.println("BUILD A FISH :");
-		new Fish();
+		System.out.println("new Fish");
+		try {
+			new Fish();
+		} catch (NullPointerException e) {
+			throw new RuntimeException(
+					"NullpointeException thrown as expected. "
+							+ "Animal calls an overridable method "
+							+ "from the constructor", e);
+		}
 	}
 }
