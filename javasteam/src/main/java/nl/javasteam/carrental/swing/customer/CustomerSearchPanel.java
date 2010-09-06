7b5797110d047fd98edea64aaa5707954b3421b6
martijn.lapre@gmail.com <martijn.lapre@gmail.com@f9376deb-ae59-c370-c37a-0523a45e0afc>
2010-09-06 09:07:57 +0000

    Initial commit customer package

---
+++ javasteam/src/main/java/nl/javasteam/carrental/swing/customer/CustomerSearchPanel.java
@@ -0,0 +1,16 @@
+package nl.javasteam.carrental.swing.customer;
+
+import javax.swing.JPanel;
+/**
+ * Customer search user interface
+ *
+ */
+public class CustomerSearchPanel extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+
+	
+	public CustomerSearchPanel() {
+		
+	}
+}
