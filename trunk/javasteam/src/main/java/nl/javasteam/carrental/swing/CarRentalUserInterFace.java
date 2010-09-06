bbb0128308bfd5eb7f7abd7d3a0dc91d6fa1ed5a
martijn.lapre@gmail.com <martijn.lapre@gmail.com@f9376deb-ae59-c370-c37a-0523a45e0afc>
2010-09-06 09:09:35 +0000

    Added a UI menuBar for closing the app

--- trunk/javasteam/src/main/java/nl/javasteam/carrental/swing/CarRentalUserInterFace.java
+++ trunk/javasteam/src/main/java/nl/javasteam/carrental/swing/CarRentalUserInterFace.java
@@ -2,6 +2,8 @@ package nl.javasteam.carrental.swing;
 
 import javax.swing.JFrame;
 
+import nl.javasteam.carrental.swing.menu.MenuBar;
+
 /**
  * CarRental User Interface
  * 
@@ -11,15 +13,24 @@ public class CarRentalUserInterFace extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private TabsPane tabsPane;
+	private MenuBar menuBar;
 
 	public CarRentalUserInterFace() {
 		setTitle("CarRental");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLayout(null);
+		
+		menuBar = new MenuBar();
+		menuBar.setBounds(0, 0, 1000, 20);
+		add(menuBar);
+		
 		tabsPane = new TabsPane();
-		tabsPane.setAlignmentY(RIGHT_ALIGNMENT);
+		tabsPane.setBounds(0, 25, 1000, 575);
 		add(tabsPane);
+		
 		pack();
 		setSize(1000, 600);
+		setLocationRelativeTo(null);
 		setVisible(true);
 	}
 }
