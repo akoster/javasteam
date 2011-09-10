731015f11ceb3ea8773646c6a978db1d74b0c613
j.a.koster@gmail.com <j.a.koster@gmail.com@f9376deb-ae59-c370-c37a-0523a45e0afc>
2011-09-10 11:40:02 +0000

    Fixed

--- trunk/javasteam/src/test/java/nl/javasteam/carrental/BaseTest.java
+++ trunk/javasteam/src/test/java/nl/javasteam/carrental/BaseTest.java
@@ -7,11 +7,10 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
-@ContextConfiguration("/applicationContext-dao.xml")
+@ContextConfiguration(locations = "/applicationContext-dao.xml")
 @RunWith(SpringJUnit4ClassRunner.class)
 @TransactionConfiguration
 @Transactional
 public abstract class BaseTest extends
 		AbstractTransactionalJUnit4SpringContextTests {
-
 }
